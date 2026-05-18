deb15f56111949630423a414e9f9879b9b59c7df
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-18 17:57:34 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // Untuk versi simple, simpan teks biasa. Untuk production nanti gunakan bcrypt.
+    password: { type: String, required: true },
     role: { type: String, default: 'user' }, // 'user' atau 'admin'
     createdAt: { type: Date, default: Date.now }
 });
