62e44d3fb20f16e0bafbcd59015dad6c252dfbaa
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-18 18:11:58 +0700

    server.js

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' })); 
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
 
-const mongoURI = process.env.MONGO_URI;
-mongoose.connect(mongoURI).then(() => console.log('✅ MongoDB Connected')).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ MongoDB Connected'))
+    .catch(err => console.log(err));
 
+// PASTIKAN BARIS INI ADA
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/users', require('./routes/userRoutes')); // Baris ini wajib ada
 
-app.get('/', (req, res) => res.send('🚀 Asvex Server is Live!'));
+app.get('/', (req, res) => res.send('API AL GADGET JALAN 🚀'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
