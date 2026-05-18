a61b1246a087ae4a2b188536246c152db3a830a2
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-18 15:19:36 +0700

    Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,41 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-    orderId: String,
-    customerName: String,
-    address: String,
-    phone: String,
-    items: Array,
-    total: Number,
-    paymentMethod: String,
-    status: { type: String, default: 'Pending' },
-    createdAt: { type: Date, default: Date.now }
+    orderId: { 
+        type: String, 
+        required: true 
+    },
+    customerName: { 
+        type: String, 
+        required: true 
+    },
+    address: { 
+        type: String, 
+        required: true 
+    },
+    items: { 
+        type: Array, 
+        required: true 
+    }, // Menyimpan daftar HP yang dibeli
+    total: { 
+        type: Number, 
+        required: true 
+    },
+    paymentMethod: { 
+        type: String, 
+        default: 'Transfer Bank' 
+    },
+    paymentProof: { 
+        type: String 
+    }, // TEMPAT MENYIMPAN FOTO BUKTI TRANSFER (BASE64)
+    status: { 
+        type: String, 
+        default: 'Pending' 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
