2e47ec5e1d5889296b8c54c73ed8cc20b3609979
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-19 17:21:54 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: 'user' }, // 'user' atau 'admin'
-    createdAt: { type: Date, default: Date.now }
+    role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' },
+    otp: { type: String, default: null },
+    otpExpires: { type: Date, default: null }
 });
-
 module.exports = mongoose.model('User', UserSchema);
