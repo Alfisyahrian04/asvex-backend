1dee4c57b9f74b217f9ef8b8ea698f4248194bc6
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-19 21:31:36 +0700

    Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose');
+
 const OrderSchema = new mongoose.Schema({
-    orderId: String, buyerEmail: String, buyerName: String, sellerId: String,
-    items: Array, total: Number, paymentProof: String, packingProof: String, unboxingProof: String,
-    status: { type: String, default: 'Waiting_Payment' },
-    createdAt: { type: Date, default: Date.now }
+    orderId: String,
+
+    buyerId: String,
+    buyerName: String,
+    buyerEmail: String,
+
+    customerName: String,
+    address: String,
+
+    sellerId: String,
+
+    items: Array,
+    total: Number,
+
+    paymentProof: String,
+    packingProof: String,
+    unboxingProof: String,
+
+    status: {
+        type: String,
+        default: 'Waiting_Payment'
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
+
 module.exports = mongoose.model('Order', OrderSchema);
