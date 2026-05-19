e5efbd65bd9c4743daf000da336a0a5276ca9dc0
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-19 17:22:54 +0700

    Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,20 +1,13 @@
 const mongoose = require('mongoose');
-
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    brand: String,
-    price: Number,
-    category: String,
-    image: String,
-    stock: Number,
+    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    sellerName: String,
+    name: String, brand: String, price: Number, category: String, image: String, stock: Number,
     description: String,
     specs: {
-        ram: String,
-        storage: String,
-        processor: String,
-        camera: String,
-        battery: String
-    }
+        ram: String, storage: String, processor: String, 
+        camFront: String, camBack: String, battery: String
+    },
+    variants: { type: Array, default: [] } // Array of colors: ["Black", "Gold"]
 });
-
 module.exports = mongoose.model('Product', ProductSchema);
