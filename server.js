c9b0535f6c7148ebfd2144b28dfb88091686261e
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-19 18:43:08 +0700

    server.js

--- server.js
+++ server.js
@@ -4,25 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// IZINKAN SEMUA KONEKSI (Sangat Penting agar HP bisa konek)
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("✅ MongoDB Connected"))
-    .catch(err => console.log("❌ DB Error:", err));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("✅ MongoDB Connected"));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
-
-app.get('/', (req, res) => res.send("AL GADGET API LIVE 🚀"));
+app.use('/api/chats', require('./routes/chatRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
