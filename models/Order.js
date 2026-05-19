365e3f74564219d795b88e73aa3a14041dbf1578
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-19 18:53:47 +0700

    Order.js

--- models/Order.js
+++ models/Order.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const OrderSchema = new mongoose.Schema({
     orderId: String, buyerEmail: String, buyerName: String, sellerId: String,
     items: Array, total: Number, paymentProof: String, packingProof: String, unboxingProof: String,
-    status: { type: String, default: 'Waiting_Payment' }, // Waiting_Payment, Escrow_Held, Shipped, Complained, Completed, Cancelled
-    complainReason: String, complainVideo: String,
+    status: { type: String, default: 'Waiting_Payment' },
     createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Order', OrderSchema);
