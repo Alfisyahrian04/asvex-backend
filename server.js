3e29f92849def57dcfa8d0fda469ef765fac76a9
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-19 18:54:34 +0700

    server.js

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("✅ MongoDB Connected"));
+// Koneksi Database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB Connected"))
+    .catch(err => console.log("❌ Error DB:", err));
 
+// Route Utama
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/chats', require('./routes/chatRoutes'));
 
+app.get('/', (req, res) => res.send("AL GADGET API LIVE 🚀"));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server on port ${PORT}`));
