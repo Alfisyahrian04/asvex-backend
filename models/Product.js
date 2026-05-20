5eceb124e07f67ca698da0f6705e7ebc9d074038
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 15:33:20 +0700

    Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
-    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    sellerName: String,
-    name: String, brand: String, price: Number, category: String, image: String, stock: Number,
-    description: String,
-    specs: { ram: String, internal: String, processor: String, camFront: String, camBack: String, battery: String },
-    variants: Array, // ["Black", "Gold"]
-    avgRating: { type: Number, default: 0 },
-    totalReviews: { type: Number, default: 0 }
+    sellerId: String, sellerName: String, name: String, brand: String, 
+    price: Number, category: String, image: String, stock: Number, description: String,
+    specs: { ram: String, internal: String, cpu: String, camFront: String, camBack: String, battery: String },
+    variants: Array, avgRating: { type: Number, default: 0 }, totalReviews: { type: Number, default: 0 }
 });
 module.exports = mongoose.model('Product', ProductSchema);
