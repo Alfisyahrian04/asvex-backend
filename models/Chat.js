9a9572da1ec13a3f87f7ab8c218ad7c3a6e6cff5
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 10:28:32 +0700

    Chat.js

--- models/Chat.js
+++ models/Chat.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
+
 const ChatSchema = new mongoose.Schema({
     roomId: String,
+
     senderId: String,
+    senderName: String,
+
     receiverId: String,
+    receiverName: String,
+
     message: String,
-    createdAt: { type: Date, default: Date.now }
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
+
 module.exports = mongoose.model('Chat', ChatSchema);
