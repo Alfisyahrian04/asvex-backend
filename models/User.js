e87e367ee551447529e25a830b5a4dc57e074c6a
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 15:28:15 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,9 @@
-const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' },
     status: { type: String, enum: ['active', 'banned'], default: 'active' },
-    bankAccount: {
-        provider: String, // BCA, Mandiri, Dana, etc
-        accountNumber: String,
-        accountName: String
-    },
-    otp: String,
-    otpExpires: Date
+    bankInfo: { provider: String, accNumber: String, accName: String },
+    walletInfo: { provider: String, phone: String, accName: String }
 });
-module.exports = mongoose.model('User', UserSchema);
