55abdb48d8685fd440120cd7c8c5546aa662900d
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 16:27:13 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
+
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' },
-    status: { type: String, enum: ['active', 'banned'], default: 'active' },
-    bankInfo: { provider: String, accNumber: String, accName: String },
-    walletInfo: { provider: String, phone: String, accName: String }
+    status: { type: String, enum: ['active', 'banned'], default: 'active' }, // Untuk fitur Suspend (Poin 12)
+    
+    // Simpan Info Bank & Wallet secara Terpisah (Poin 6)
+    bankInfo: {
+        provider: { type: String, default: null }, // BCA, Mandiri, dll
+        accNumber: { type: String, default: null },
+        accName: { type: String, default: null }
+    },
+    walletInfo: {
+        provider: { type: String, default: null }, // Dana, Gopay, dll
+        phone: { type: String, default: null },
+        accName: { type: String, default: null }
+    },
+    
+    otp: { type: String, default: null },
+    otpExpires: { type: Date, default: null }
 });
+
 module.exports = mongoose.model('User', UserSchema);
