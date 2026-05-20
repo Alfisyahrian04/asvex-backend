2be416bb1e7c8c4176c2770a81796c85b95fc33a
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 15:44:03 +0700

    Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,11 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // BARIS INI WAJIB ADA!
+
 const OrderSchema = new mongoose.Schema({
-    orderId: String, buyerId: String, buyerEmail: String, buyerName: String, buyerPhone: String,
-    customerName: String, address: String, sellerId: String, sellerName: String,
-    items: Array, total: Number, paymentProof: String, packingProof: String, unboxingProof: String,
-    adminTransferProof: String, status: { type: String, default: 'Waiting_Payment' },
+    orderId: String,
+    buyerId: String,
+    buyerEmail: String,
+    buyerName: String,
+    buyerPhone: String,
+    customerName: String,
+    address: String,
+    sellerId: String,
+    sellerName: String,
+    items: Array,
+    total: Number,
+    paymentProof: String,
+    packingProof: String,
+    unboxingProof: String,
+    adminTransferProof: String,
+    status: { type: String, default: 'Waiting_Payment' },
     isRated: { type: Boolean, default: false },
-    review: { rating: Number, comment: String, date: Date },
+    review: {
+        rating: Number,
+        comment: String,
+        date: Date
+    },
     createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Order', OrderSchema);
