31c07f505929d99146cafbe45531c5905fc4605c
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 15:32:34 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -5,5 +6,8 @@ const UserSchema = new mongoose.Schema({
     role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' },
     status: { type: String, enum: ['active', 'banned'], default: 'active' },
     bankInfo: { provider: String, accNumber: String, accName: String },
-    walletInfo: { provider: String, phone: String, accName: String }
+    walletInfo: { provider: String, phone: String, accName: String },
+    otp: String,
+    otpExpires: Date
 });
+module.exports = mongoose.model('User', UserSchema);
