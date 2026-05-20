808a21ae958cf218a284ae8d3cf10c421f65b1c4
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-20 15:43:23 +0700

    Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
+
 const ProductSchema = new mongoose.Schema({
-    sellerId: String, sellerName: String, name: String, brand: String, 
-    price: Number, category: String, image: String, stock: Number, description: String,
-    specs: { ram: String, internal: String, cpu: String, camFront: String, camBack: String, battery: String },
-    variants: Array, avgRating: { type: Number, default: 0 }, totalReviews: { type: Number, default: 0 }
+    sellerId: String,
+    sellerName: String,
+    name: String,
+    brand: String,
+    price: Number,
+    category: String,
+    image: String,
+    stock: Number,
+    description: String,
+    specs: {
+        ram: String,
+        internal: String,
+        cpu: String,
+        camFront: String,
+        camBack: String,
+        battery: String
+    },
+    variants: { type: Array, default: [] },
+    avgRating: { type: Number, default: 0 },
+    totalReviews: { type: Number, default: 0 }
 });
+
 module.exports = mongoose.model('Product', ProductSchema);
