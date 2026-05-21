3624c98a99c5d1bc37f8e2341321c3e9a352a74f
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-21 23:34:31 +0700

    Update stockServices.js

--- phone-marketplace/backend/src/services/stockServices.js
+++ phone-marketplace/backend/src/services/stockServices.js
@@ -1,1 +1,41 @@
+const Product =
+require('../models/Product');
 
+exports.reduceStock =
+async items => {
+
+  for (const item of items) {
+
+    const product =
+      await Product.findById(
+        item.productId
+      );
+
+    if (!product) {
+
+      throw new Error(
+        'Product tidak ditemukan'
+      );
+
+    }
+
+    if (
+      product.stock <
+      item.qty
+    ) {
+
+      throw new Error(
+        'Stock tidak cukup'
+      );
+
+    }
+
+    product.stock -= item.qty;
+
+    product.totalSold += item.qty;
+
+    await product.save();
+
+  }
+
+};
