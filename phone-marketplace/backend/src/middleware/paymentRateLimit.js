4902de198152eb84bf8044c4cc68d19a7271d661
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-21 23:21:12 +0700

    Create paymentRateLimit.js

---
+++ phone-marketplace/backend/src/middleware/paymentRateLimit.js
@@ -0,0 +1,21 @@
+const rateLimit =
+require('express-rate-limit');
+
+module.exports =
+rateLimit({
+
+  windowMs:
+    60 * 1000,
+
+  max: 5,
+
+  message: {
+
+    success: false,
+
+    message:
+      'Terlalu banyak pembayaran'
+
+  }
+
+});
