1c8fc678ad50e2629d0ee8227658aa653a5ff3a3
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-21 23:10:55 +0700

    Update User.js

--- phone-marketplace/backend/src/models/User.js
+++ phone-marketplace/backend/src/models/User.js
@@ -1,1 +1,66 @@
+const mongoose =
+require('mongoose');
 
+const bcrypt =
+require('bcryptjs');
+
+const schema =
+new mongoose.Schema({
+
+  username: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  role: {
+    type: String,
+    enum: [
+      'buyer',
+      'seller',
+      'admin'
+    ],
+    default: 'buyer'
+  }
+
+}, {
+  timestamps: true
+});
+
+schema.pre(
+  'save',
+  async function(next) {
+
+    if (
+      !this.isModified(
+        'password'
+      )
+    ) {
+      return next();
+    }
+
+    this.password =
+    await bcrypt.hash(
+      this.password,
+      10
+    );
+
+    next();
+
+  }
+);
+
+module.exports =
+mongoose.model(
+  'User',
+  schema
+);
