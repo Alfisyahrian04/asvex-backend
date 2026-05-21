0b7e7b5a64515aa10612277bd8998b389041dbbe
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-21 23:09:04 +0700

    Update db.js

--- phone-marketplace/backend/src/config/db.js
+++ phone-marketplace/backend/src/config/db.js
@@ -1,1 +1,15 @@
+const mongoose =
+require('mongoose');
 
+module.exports =
+async () => {
+
+  await mongoose.connect(
+    process.env.MONGO_URI
+  );
+
+  console.log(
+    'MongoDB Connected'
+  );
+
+};
