28c1aa8351bec0fd62c51a9db7e07205a31490ae
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-21 23:07:09 +0700

    Update server.js

--- phone-marketplace/backend/src/server.js
+++ phone-marketplace/backend/src/server.js
@@ -1,1 +1,27 @@
+require('dotenv').config();
 
+const http = require('http');
+
+const app = require('./app');
+
+const connectDB = require('./config/db');
+
+const initSocket = require('./config/socket');
+
+(async () => {
+
+  await connectDB();
+
+  const server = http.createServer(app);
+
+  initSocket(server);
+
+  server.listen(process.env.PORT, () => {
+
+    console.log(
+      `SERVER RUNNING ${process.env.PORT}`
+    );
+
+  });
+
+})();
