28ec4195702f1ce374c1eff6c8725f99e724a3f3
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-21 23:36:42 +0700

    Create shippingService.js

---
+++ phone-marketplace/backend/src/services/shippingService.js
@@ -0,0 +1,18 @@
+exports.calculateShipping =
+async ({
+  weight,
+  destination
+}) => {
+
+  return {
+
+    courier: 'JNE',
+
+    price: 18000,
+
+    estimation:
+      '2-3 Hari'
+
+  };
+
+};
