d3cbde6bf17a8dd5ee5e33fd669c9e1b2f8e9cb3
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 00:16:31 +0700

    Update productQuery.js

--- phone-marketplace/backend/src/queries/productQuery.js
+++ phone-marketplace/backend/src/queries/productQuery.js
@@ -1,1 +1,16 @@
+const Product =
+require('../models/Product');
 
+exports.findProducts =
+async ({
+  limit = 20,
+  skip = 0
+}) => {
+
+  return await Product.find()
+
+    .limit(limit)
+
+    .skip(skip);
+
+};
