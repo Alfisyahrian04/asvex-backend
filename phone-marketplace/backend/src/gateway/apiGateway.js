3b8a8c865f49cfc95c710358739661e401926db8
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 00:15:20 +0700

    Update apiGateway.js

--- phone-marketplace/backend/src/gateway/apiGateway.js
+++ phone-marketplace/backend/src/gateway/apiGateway.js
@@ -1,1 +1,31 @@
+const express =
+require('express');
 
+const proxy =
+require('express-http-proxy');
+
+const app =
+express();
+
+app.use(
+  '/auth',
+  proxy(
+    process.env.AUTH_SERVICE
+  )
+);
+
+app.use(
+  '/product',
+  proxy(
+    process.env.PRODUCT_SERVICE
+  )
+);
+
+app.use(
+  '/order',
+  proxy(
+    process.env.ORDER_SERVICE
+  )
+);
+
+module.exports = app;
