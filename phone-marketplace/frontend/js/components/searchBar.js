d569b872e11836519cfd6cd421e0f7744f84086e
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 16:30:56 +0700

    Create searchBar.js

---
+++ phone-marketplace/frontend/js/components/searchBar.js
@@ -0,0 +1,17 @@
+export default function searchBar() {
+
+  return `
+
+    <div class="w-full">
+
+      <input
+        type="text"
+        placeholder="Cari produk..."
+        class="w-full border p-4 rounded-2xl"
+      />
+
+    </div>
+
+  `;
+
+}
