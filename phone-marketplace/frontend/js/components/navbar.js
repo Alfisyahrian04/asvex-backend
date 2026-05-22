4797623377f4f792d2b49ea8c98ac356e7769e58
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 15:53:15 +0700

    Update navbar.js

--- phone-marketplace/frontend/js/components/navbar.js
+++ phone-marketplace/frontend/js/components/navbar.js
@@ -1,1 +1,36 @@
+export default function navbar() {
 
+  return `
+
+    <nav class="navbar">
+
+      <a
+        href="/index.html"
+        class="text-2xl font-bold text-green-600"
+      >
+        PHONE MARKET
+      </a>
+
+      <input
+        type="text"
+        placeholder="Cari HP..."
+        class="border p-3 rounded-2xl w-full max-w-xl"
+      />
+
+      <div class="flex gap-3">
+
+        <a href="/orders.html">
+          Orders
+        </a>
+
+        <a href="/login.html">
+          Login
+        </a>
+
+      </div>
+
+    </nav>
+
+  `;
+
+}
