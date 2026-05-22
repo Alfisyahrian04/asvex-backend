ebcf9e10589b833a258b69c8b57d2b50b9b4a232
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 15:50:19 +0700

    Update authStore.js

--- phone-marketplace/frontend/js/store/authStore.js
+++ phone-marketplace/frontend/js/store/authStore.js
@@ -1,1 +1,46 @@
+const authStore = {
 
+  getUser() {
+
+    return JSON.parse(
+      localStorage.getItem(
+        'user'
+      )
+    );
+
+  },
+
+  getToken() {
+
+    return localStorage.getItem(
+      'token'
+    );
+
+  },
+
+  isLoggedIn() {
+
+    return !!localStorage.getItem(
+      'token'
+    );
+
+  },
+
+  logout() {
+
+    localStorage.removeItem(
+      'token'
+    );
+
+    localStorage.removeItem(
+      'user'
+    );
+
+    location.href =
+      '/login.html';
+
+  }
+
+};
+
+export default authStore;
