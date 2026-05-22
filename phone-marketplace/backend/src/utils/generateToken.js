a63df04418037621f36630144a3c0724d4e076e2
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 16:27:36 +0700

    Update generateToken.js

--- phone-marketplace/backend/src/utils/generateToken.js
+++ phone-marketplace/backend/src/utils/generateToken.js
@@ -1,1 +1,21 @@
+const jwt =
+require('jsonwebtoken');
 
+module.exports =
+payload => {
+
+  return jwt.sign(
+
+    payload,
+
+    process.env.JWT_SECRET,
+
+    {
+
+      expiresIn: '7d'
+
+    }
+
+  );
+
+};
