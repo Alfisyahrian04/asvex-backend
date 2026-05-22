1ed18299bd75815ca171b2ff68525225b791d8c4
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 15:43:04 +0700

    Update api.js

--- phone-marketplace/frontend/js/api/api.js
+++ phone-marketplace/frontend/js/api/api.js
@@ -1,1 +1,41 @@
+export const BASE_URL =
+'https://asvex-backend-production.up.railway.app/api/v1';
 
+export async function request(
+  endpoint,
+  options = {}
+) {
+
+  const token =
+    localStorage.getItem(
+      'token'
+    );
+
+  const response =
+    await fetch(
+
+      `${BASE_URL}${endpoint}`,
+
+      {
+
+        ...options,
+
+        headers: {
+
+          'Content-Type':
+            'application/json',
+
+          Authorization:
+            token
+            ? `Bearer ${token}`
+            : ''
+
+        }
+
+      }
+
+    );
+
+  return response.json();
+
+}
