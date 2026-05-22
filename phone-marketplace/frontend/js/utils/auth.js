cc2020207d91eda4be6f928056e0645747b9ef19
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 16:01:27 +0700

    Update auth.js

--- phone-marketplace/frontend/js/utils/auth.js
+++ phone-marketplace/frontend/js/utils/auth.js
@@ -1,1 +1,32 @@
+export function getUser() {
 
+  return JSON.parse(
+    localStorage.getItem(
+      'user'
+    )
+  );
+
+}
+
+export function isLoggedIn() {
+
+  return !!localStorage.getItem(
+    'token'
+  );
+
+}
+
+export function logout() {
+
+  localStorage.removeItem(
+    'token'
+  );
+
+  localStorage.removeItem(
+    'user'
+  );
+
+  location.href =
+    '/login.html';
+
+}
