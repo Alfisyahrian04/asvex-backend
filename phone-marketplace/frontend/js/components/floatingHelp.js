6e638f1914347804d3967fd45a439e02e1d50fc1
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 15:57:31 +0700

    Create floatingHelp.js

---
+++ phone-marketplace/frontend/js/components/floatingHelp.js
@@ -0,0 +1,15 @@
+export default function floatingHelp() {
+
+  return `
+
+    <button
+      class="fixed bottom-24 right-6 bg-green-500 text-white w-16 h-16 rounded-full shadow-lg"
+    >
+
+      ?
+
+    </button>
+
+  `;
+
+}
