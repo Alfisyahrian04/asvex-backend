80e414ee656ab2076851f3a88a3597af169de968
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-22 15:55:15 +0700

    Create bottomNav.js

---
+++ phone-marketplace/frontend/js/components/bottomNav.js
@@ -0,0 +1,27 @@
+export default function bottomNav() {
+
+  return `
+
+    <div class="fixed bottom-0 left-0 right-0 bg-white shadow-lg flex justify-around p-4">
+
+      <a href="/index.html">
+        Home
+      </a>
+
+      <a href="/orders.html">
+        Orders
+      </a>
+
+      <a href="/seller.html">
+        Seller
+      </a>
+
+      <a href="/admin.html">
+        Admin
+      </a>
+
+    </div>
+
+  `;
+
+}
