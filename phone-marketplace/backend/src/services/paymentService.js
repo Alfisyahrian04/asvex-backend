294a76b7f3abf7c18891e3dae10de96eb993d8f5
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-23 13:22:48 +0700

    Update paymentService.js

--- phone-marketplace/backend/src/services/paymentService.js
+++ phone-marketplace/backend/src/services/paymentService.js
@@ -1,12 +1,55 @@
+const midtransClient =
+require('midtrans-client');
+
+const snap =
+new midtransClient.Snap({
+
+isProduction:false,
+
+serverKey:
+process.env.MIDTRANS_SERVER_KEY,
+
+clientKey:
+process.env.MIDTRANS_CLIENT_KEY
+
+});
+
 exports.createPayment =
 async(order)=>{
 
+const parameter = {
+
+transaction_details:{
+
+order_id:
+order._id.toString(),
+
+gross_amount:
+order.totalPrice
+
+},
+
+customer_details:{
+
+first_name:
+order.buyer.toString()
+
+}
+
+};
+
+const transaction =
+await snap.createTransaction(
+parameter
+);
+
 return {
 
-paymentUrl:
-'https://payment-gateway.com',
+token:
+transaction.token,
 
-status:'pending'
+redirect_url:
+transaction.redirect_url
 
 };
 
