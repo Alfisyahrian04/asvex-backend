b0c5a6440a50f68ba3cc515e3e10bdfe6723451f
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-23 15:06:37 +0700

    Update User.js

--- phone-marketplace/backend/src/models/User.js
+++ phone-marketplace/backend/src/models/User.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose =
+require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const UserSchema =
+new mongoose.Schema({
 
 username:{
 type:String,
@@ -58,7 +60,15 @@ default:false
 isBanned:{
 type:Boolean,
 default:false
+},
+
+wishlist:[
+{
+type:
+mongoose.Schema.Types.ObjectId,
+ref:'Product'
 }
+]
 
 },{
 timestamps:true
