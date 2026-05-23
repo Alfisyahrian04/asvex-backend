6cc2c507182cda6a409cbcbc4cfd2c442a533a19
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-23 12:43:29 +0700

    Update productController.js

--- phone-marketplace/backend/src/controllers/productController.js
+++ phone-marketplace/backend/src/controllers/productController.js
@@ -1,35 +1,207 @@
 const Product =
 require('../models/Product');
 
-exports.getProducts =
-async (req, res) => {
+exports.createProduct =
+async(req,res)=>{
 
-  const products =
-    await Product.find()
-    .sort({
-      createdAt: -1
-    });
+try{
 
-  res.json(products);
+const product =
+await Product.create({
+
+...req.body,
+seller:req.user._id
+
+});
+
+res.status(201)
+.json(product);
+
+}catch(error){
+
+res.status(500)
+.json({
+message:error.message
+});
+
+}
 
 };
 
-exports.createProduct =
-async (req, res) => {
+exports.getProducts =
+async(req,res)=>{
 
-  const product =
-    await Product.create({
+try{
 
-      ...req.body,
+const products =
+await Product.find({
+isActive:true
+})
+.populate(
+'seller',
+'username'
+)
+.sort({
+createdAt:-1
+});
 
-      sellerId:
-        req.user._id,
+res.json(products);
 
-      sellerName:
-        req.user.username
+}catch(error){
 
-    });
+res.status(500)
+.json({
+message:error.message
+});
 
-  res.json(product);
+}
+
+};
+
+exports.getSingleProduct =
+async(req,res)=>{
+
+try{
+
+const product =
+await Product.findById(
+req.params.id
+).populate(
+'seller',
+'username'
+);
+
+if(!product){
+
+return res.status(404)
+.json({
+message:
+'Product not found'
+});
+
+}
+
+res.json(product);
+
+}catch(error){
+
+res.status(500)
+.json({
+message:error.message
+});
+
+}
+
+};
+
+exports.updateProduct =
+async(req,res)=>{
+
+try{
+
+const product =
+await Product.findById(
+req.params.id
+);
+
+if(!product){
+
+return res.status(404)
+.json({
+message:
+'Product not found'
+});
+
+}
+
+if(
+product.seller.toString()
+!== req.user._id.toString()
+){
+
+return res.status(403)
+.json({
+message:
+'Forbidden'
+});
+
+}
+
+const updatedProduct =
+await Product.findByIdAndUpdate(
+
+req.params.id,
+
+req.body,
+
+{
+new:true
+}
+
+);
+
+res.json(updatedProduct);
+
+}catch(error){
+
+res.status(500)
+.json({
+message:error.message
+});
+
+}
+
+};
+
+exports.deleteProduct =
+async(req,res)=>{
+
+try{
+
+const product =
+await Product.findById(
+req.params.id
+);
+
+if(!product){
+
+return res.status(404)
+.json({
+message:
+'Product not found'
+});
+
+}
+
+if(
+product.seller.toString()
+!== req.user._id.toString()
+){
+
+return res.status(403)
+.json({
+message:
+'Forbidden'
+});
+
+}
+
+product.isActive = false;
+
+await product.save();
+
+res.json({
+message:
+'Product deleted'
+});
+
+}catch(error){
+
+res.status(500)
+.json({
+message:error.message
+});
+
+}
 
 };
