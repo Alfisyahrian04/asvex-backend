933ceeecf8458adf062f451d8c1222e62750a16e
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-24 11:06:41 +0700

    Update User.js

--- phone-marketplace/backend/src/models/User.js
+++ phone-marketplace/backend/src/models/User.js
@@ -1,6 +1,53 @@
 const mongoose =
 require('mongoose');
 
+/* PATCH */
+const AddressSchema =
+new mongoose.Schema({
+
+label:{
+type:String,
+default:'Rumah'
+},
+
+receiverName:{
+type:String,
+default:''
+},
+
+phone:{
+type:String,
+default:''
+},
+
+address:{
+type:String,
+default:''
+},
+
+city:{
+type:String,
+default:''
+},
+
+province:{
+type:String,
+default:''
+},
+
+postalCode:{
+type:String,
+default:''
+},
+
+isDefault:{
+type:Boolean,
+default:false
+}
+
+},{ _id:true });
+/* PATCH */
+
 const UserSchema =
 new mongoose.Schema({
 
@@ -9,6 +56,13 @@ type:String,
 required:true,
 unique:true
 },
+
+/* PATCH */
+fullName:{
+type:String,
+default:''
+},
+/* PATCH */
 
 email:{
 type:String,
@@ -46,6 +100,30 @@ address:{
 type:String,
 default:''
 },
+
+/* PATCH START */
+
+addresses:{
+type:[AddressSchema],
+default:[]
+},
+
+defaultAddressId:{
+type:String,
+default:''
+},
+
+storeName:{
+type:String,
+default:''
+},
+
+returnAddress:{
+type:String,
+default:''
+},
+
+/* PATCH END */
 
 walletBalance:{
 type:Number,
