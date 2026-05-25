40d932359c55eca5e75fdd96ebc35e8e40f86167
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-25 11:02:43 +0700

    Update productCard.js

--- phone-marketplace/frontend/js/components/productCard.js
+++ phone-marketplace/frontend/js/components/productCard.js
@@ -18,35 +18,29 @@ alt="${product.name}"
 
 <div class="product-content">
 
+<p class="product-category">
+${product.category || '-'}
+</p>
+
 <h3>
 ${product.name}
 </h3>
 
-<p class="product-category">
-${product.category}
-</p>
-
 <p class="product-price">
-Rp ${Number(product.price)
+Rp ${Number(product.price || 0)
 .toLocaleString('id-ID')}
 </p>
 
 <div class="product-rating">
-
 ⭐ ${product.rating || 0}
-(
-${product.reviewCount || 0}
-)
-
+(${product.reviewCount || 0})
 </div>
 
 <button
 class="add-cart-btn"
 onclick='addToCart(${JSON.stringify(product)})'
 >
-
 Tambah Keranjang
-
 </button>
 
 </div>
