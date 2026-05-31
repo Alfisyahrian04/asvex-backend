2772811c96e18c8a653c6b7495b76175d875a621
Alfisyahrian04 <asyahrian46@gmail.com>
2026-05-31 14:09:00 +0700

    Update adminController.js

--- phone-marketplace/backend/src/controllers/adminController.js
+++ phone-marketplace/backend/src/controllers/adminController.js
@@ -640,11 +640,37 @@ message:error.message
 }
 };
 
+const getAllOrders =
+async(req,res)=>{
+try{
+
+const orders =
+await Order.find()
+.populate('buyer','username')
+.populate('seller','username storeName')
+.populate('product','name')
+.sort({ createdAt:-1 });
+
+res.status(200).json({
+success:true,
+orders
+});
+
+}catch(error){
+
+res.status(500).json({
+message:error.message
+});
+
+}
+};
+
 module.exports = {
 
 getAllUsers,
 verifySeller,
 banUser,
+getAllOrders,
 
 getPayouts,
 approvePayout,
